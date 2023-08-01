1b9033ac8235e64dc659763eaf99e83061b6966b
Chamy619 <yd95069506@naver.com>
2023-08-01 23:35:06 +0900

    feat: 반응형 적용

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ const gallery = new Viewer(document.getElementById('images'), {
   //   hidden: true,
 });
 
+const $door = document.getElementById('door');
+$door.style.backgroundImage =
+  'url("https://d6cjuwf9jj9iv.cloudfront.net/milkyway.jpeg")';
+
 // gallery.show();
